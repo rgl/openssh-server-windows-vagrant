adc3e9d735b3b49779069ac7862f37cb9e568bf0
Rui Lopes <rgl@ruilopes.com>
2020-08-27 19:43:11 +0100

    upgrade to adoptopenjdk 11 and gradle 6.6

--- examples/java/run.ps1
+++ examples/java/run.ps1
@@ -1,13 +1,5 @@
 # install dependencies.
-choco install -y jdk8 gradle
-
-# enable the Java Cryptography Extension (JCE) Unlimited Strength Jurisdiction Policy.
-# see https://raw.githubusercontent.com/rgl/jce-chocolatey-package/master/tools/chocolateyInstall.ps1
-$packageName = 'jdk8'
-$javaRegistryKeyName = if ($packageName -like 'jre*') {'Java Runtime Environment'} else {'Java Development Kit'}
-$javaHome = (Get-ItemProperty -Path "HKLM:\SOFTWARE\JavaSoft\$javaRegistryKeyName\1.8" -Name JavaHome).JavaHome
-$jreHome = if ($packageName -like 'jre*') {$javaHome} else {"$javaHome\jre"}
-Copy-Item -Force "$jreHome\lib\security\policy\unlimited\*" "$jreHome\lib\security"
+choco install -y adoptopenjdk11 gradle
 
 # update $env:PATH with the recently installed Chocolatey packages.
 Import-Module C:\ProgramData\chocolatey\helpers\chocolateyInstaller.psm1
