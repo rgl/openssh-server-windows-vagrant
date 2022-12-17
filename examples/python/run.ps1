4809d78f72448b613e760b2863b247b093921765
Rui Lopes <rgl@ruilopes.com>
2022-12-17 20:27:01 +0000

    upgrade to python 3.11.0

--- examples/python/run.ps1
+++ examples/python/run.ps1
@@ -1,5 +1,6 @@
 # install dependencies.
-choco install -y python --version 3.10.1
+# see https://community.chocolatey.org/packages/python
+choco install -y python --version 3.11.0
 
 # update $env:PATH with the recently installed Chocolatey packages.
 Import-Module C:\ProgramData\chocolatey\helpers\chocolateyInstaller.psm1
