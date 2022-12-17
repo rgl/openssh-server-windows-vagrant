85d39ee870657005e7e4e9778c1a8453151ba81f
Rui Lopes <rgl@ruilopes.com>
2022-12-17 20:26:23 +0000

    upgrade to java example dependencies

--- examples/java/run.ps1
+++ examples/java/run.ps1
@@ -1,6 +1,8 @@
 # install dependencies.
-choco install -y adoptopenjdk11
-choco install -y gradle --version 7.3
+# see https://community.chocolatey.org/packages/temurin11
+choco install -y temurin11
+# see https://community.chocolatey.org/packages/gradle
+choco install -y gradle --version 7.5.1
 
 # update $env:PATH with the recently installed Chocolatey packages.
 Import-Module C:\ProgramData\chocolatey\helpers\chocolateyInstaller.psm1
