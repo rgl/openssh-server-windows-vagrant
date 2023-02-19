ba05589c3123311e0480f7b5764f0286dbd9db7a
Rui Lopes <rgl@ruilopes.com>
2023-02-19 13:14:08 +0000

    upgrade to powershell 7.2.9

--- provision-powershell.ps1
+++ provision-powershell.ps1
@@ -1,16 +1,13 @@
 # install powershell lts.
 # see https://github.com/PowerShell/PowerShell/releases
-$archiveUrl = 'https://github.com/PowerShell/PowerShell/releases/download/v7.2.8/PowerShell-7.2.8-win-x64.msi'
-$archiveHash = '9a27962c3652c85193c6f3678741a712e880aa9f85f403774cd632c5cb8680ff'
+# renovate: datasource=github-releases depName=PowerShell/PowerShell extractVersion=^(?<version>7\.2\..+)
+$archiveVersion = '7.2.9'
+$archiveUrl = "https://github.com/PowerShell/PowerShell/releases/download/v$archiveVersion/PowerShell-$archiveVersion-win-x64.msi"
 $archiveName = Split-Path -Leaf $archiveUrl
 $archivePath = "$env:TEMP\$archiveName"
 
 Write-Host "Downloading $archiveName..."
 (New-Object Net.WebClient).DownloadFile($archiveUrl, $archivePath)
-$archiveActualHash = (Get-FileHash $archivePath -Algorithm SHA256).Hash
-if ($archiveHash -ne $archiveActualHash) {
-    throw "$archiveName downloaded from $archiveUrl to $archivePath has $archiveActualHash hash witch does not match the expected $archiveHash"
-}
 
 Write-Host "Installing $archiveName..."
 msiexec /i $archivePath `
