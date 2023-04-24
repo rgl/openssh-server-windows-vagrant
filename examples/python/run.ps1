bd6b37afc6bbb95d77e8e3f4379c6512a5d7b9d3
Rui Lopes <rgl@ruilopes.com>
2023-04-24 18:09:30 +0100

    upgrade to python 3.11.3

--- examples/python/run.ps1
+++ examples/python/run.ps1
@@ -1,7 +1,7 @@
 # install dependencies.
 # see https://community.chocolatey.org/packages/python
 # renovate: datasource=nuget:chocolatey depName=python
-choco install -y python --version '3.11.0'
+choco install -y python --version '3.11.3'
 
 # update $env:PATH with the recently installed Chocolatey packages.
 Import-Module C:\ProgramData\chocolatey\helpers\chocolateyInstaller.psm1
