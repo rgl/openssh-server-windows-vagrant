f049b28c68fc946bbe65acb3137f20166320aa5c
Rui Lopes <rgl@ruilopes.com>
2023-04-24 18:10:47 +0100

    upgrade to dotnet sdk 6.0.408 (dotnet 6.0.16)

--- examples/csharp/run.ps1
+++ examples/csharp/run.ps1
@@ -1,7 +1,7 @@
 # see https://community.chocolatey.org/packages/dotnet-6.0-sdk
 # see https://dotnet.microsoft.com/en-us/download/dotnet/6.0
 # renovate: datasource=nuget:chocolatey depName=dotnet-6.0-sdk
-choco install -y dotnet-6.0-sdk --version '6.0.406'
+choco install -y dotnet-6.0-sdk --version '6.0.408'
 
 # update $env:PATH with the recently installed Chocolatey packages.
 Import-Module C:\ProgramData\chocolatey\helpers\chocolateyInstaller.psm1
