9766bc024b80bf5d8de418f783430fa694356033
Rui Lopes <rgl@ruilopes.com>
2026-02-14 20:29:07 +0000

    WIP upgrade to rsync shim

--- ps.ps1
+++ ps.ps1
@@ -61,23 +61,49 @@ $openSshHome = 'C:\Program Files\OpenSSH'
 
 function Install-RsyncBinaries {
     if (Test-Path $rsyncHome) {
+        Write-Host "Removing the existing $rsyncHome..."
         Remove-Item -Force -Recurse $rsyncHome
     }
-    # see https://github.com/rgl/rsync-vagrant/releases
-    # renovate: datasource=github-releases depName=rgl/rsync-vagrant
-    $version = '3.4.1-20250411'
-    Install-ZippedApplication `
-        $rsyncHome `
-        rsync `
-        "https://github.com/rgl/rsync-vagrant/releases/download/v$version/rsync-vagrant-$version.zip" `
-        8a4492df681a66074b59513f4446ef660fe451187b2c943096fcde46a5c17a21
-    &"$rsyncHome\rsync.exe" --version
+    if (Test-Path c:\vagrant\tmp\rsync-vagrant-*.zip) {
+        $rsyncArtifact = Resolve-Path c:\vagrant\tmp\rsync-vagrant-*.zip
+        Write-Host "Installing rsync from $rsyncArtifact..."
+        Expand-Archive $rsyncArtifact $rsyncHome
+    } else {
+        # see https://github.com/rgl/rsync-vagrant/releases
+        # renovate: datasource=github-releases depName=rgl/rsync-vagrant
+        $version = '3.4.1-20250411'
+        $url = "https://github.com/rgl/rsync-vagrant/releases/download/v$version/rsync-vagrant-$version.zip"
+        Write-Host "Installing rsync from $url..."
+        Install-ZippedApplication `
+            $rsyncHome `
+            rsync `
+            $url `
+            8a4492df681a66074b59513f4446ef660fe451187b2c943096fcde46a5c17a21
+    }
+    $rsyncCmdPath = if (Test-Path "$rsyncHome\cmd\rsync.exe") {
+        "$rsyncHome\cmd"
+    } else {
+        $rsyncHome
+    }
+    &"$rsyncCmdPath\rsync.exe" --version
     $systemPath = [Environment]::GetEnvironmentVariable('PATH', 'Machine') -split ';'
-    if ($systemPath -notcontains $rsyncHome) {
-        Write-Host "Adding $rsyncHome to the system PATH..."
+    if ($rsyncHome -ne $rsyncCmdPath -and $systemPath -contains $rsyncHome) {
+        $systemPath = $systemPath -ne $rsyncHome
+        Write-Host "Removing $rsyncHome to the system PATH..."
         [Environment]::SetEnvironmentVariable(
             'PATH',
-            "$([Environment]::GetEnvironmentVariable('PATH', 'Machine'));$rsyncHome",
+            "$($systemPath -join ';')",
+            'Machine')
+    }
+    if ($systemPath -notcontains $rsyncCmdPath) {
+        Write-Host "Adding $rsyncCmdPath to the system PATH..."
+        $systemPath = @(
+            $systemPath
+            $rsyncCmdPath
+        )
+        [Environment]::SetEnvironmentVariable(
+            'PATH',
+            "$($systemPath -join ';')",
             'Machine')
     }
 }
@@ -111,14 +137,15 @@ function Install-OpenSshBinaries {
             Remove-Item -Recurse $openSshHome -ErrorAction SilentlyContinue
         }
     }
-    Write-Host 'Installing Win32-OpenSSH...'
     # see https://github.com/PowerShell/Win32-OpenSSH/releases
     # renovate: datasource=github-releases depName=PowerShell/Win32-OpenSSH
     $version = '10.0.0.0p2-Preview'
+    $url = "https://github.com/PowerShell/Win32-OpenSSH/releases/download/$version/OpenSSH-Win64.zip"
+    Write-Host "Installing Win32-OpenSSH from $url..."
     Install-ZippedApplication `
         $openSshHome `
         OpenSSH `
-        "https://github.com/PowerShell/Win32-OpenSSH/releases/download/$version/OpenSSH-Win64.zip" `
+        $url `
         23f50f3458c4c5d0b12217c6a5ddfde0137210a30fa870e98b29827f7b43aba5
     Push-Location $openSshHome
     Move-Item OpenSSH-Win64\* .
